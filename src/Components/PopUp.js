20b0474c5ea71d9318aa80c3a670bf53932cb39f
Rahul Bhalchandra Sahastrabuddhe <rahulsahastrabuddhe7@gmail.com>
2021-06-30 10:02:06 -0500

    Added payment feature

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -1,28 +1,67 @@
-import React, { Component } from "react";
-export default class PopUp extends Component {
-  handleClick = () => {
-   this.props.toggle();
-  };
-render() {
+import React from "react";
+
+export default function PopUp(props) {
+  const [paid, setPaid] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const paypalRef = React.useRef();
+
+  const handleClick = () => {
+    props.toggle();
+   };
+  // To show PayPal buttons once the component loads
+  React.useEffect(() => {
+    window.paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            intent: "CAPTURE",
+            purchase_units: [
+              {
+                description: "IAS Membership",
+                amount: {
+                  currency_code: "USD",
+                  value: 5.0,
+                },
+              },
+            ],
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          setPaid(true);
+          console.log(order);
+        },
+        onError: (err) => {
+        //   setError(err),
+          console.error(err);
+        },
+      })
+      .render(paypalRef.current);
+  }, []);
+
+  // If the payment has been made
+  if (paid) {
+    return <div>Payment successful.!</div>;
+  }
+
+  // If any error occurs
+  if (error) {
+    return <div>Error Occurred in processing payment.! Please try again.</div>;
+  }
+
+  // Default Render
   return (
-      <div className="popup">
-   <div className="modal">
+    <div className="popup">
+    <div className="modal">
      <div className="modal_content">
-     <span className="close" onClick={this.handleClick}>&times;    </span>
+     <span className="close" onClick={handleClick}>&times;    </span>
      <h3>Become A Member</h3>
-     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-     <input type="hidden" name="cmd" value="_s-xclick"></input>
-     <input type="text" name="os0" maxlength="200">Name:</input>
-     <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHbwYJKoZIhvcNAQcEoIIHYDCCB1wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCjcSB+vrUI0K4KdFhB2t5f7uwUq6nltrg1qY1+RBBrEuYcOjrZNJnVXJwoMYDIC4tfHTgLii0035gSp4KHKlGLhuevCdJD8P9akbR+jSPAKiDB1fVfRzkD+DMDyG7SwememRBAQJngvlXdi26Vs+ezgbOkAq6o2xIGilmN6vq68zELMAkGBSsOAwIaBQAwgewGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIXCC922m1+X2AgcjbzF7jep3xaUiFrUN57t3ZvzfyN74B1ybyRhxTeqKoZvMz25+ql3PjNeWnNvE0I8GlHgGzsjKhTMN/BMiUsdR+/t/qkNoNtrkN6TjflFsYl31wnp7sb/0aQiNMW8ZkQU6d4EdNOL0QXVPUG+RpyHF/+N5AikM3HjNO5AVRtqHkDM/8Y0HpeByVL1wGuZOuEQGhL+d+QVCuiaHtAPK4nI2wfmzC/GJaoNr7SIrSGZdQC/1M5sUxW3BToSHtx9l0mfPbcwe1edL8cqCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTIxMDYyMTIxNTUwNVowIwYJKoZIhvcNAQkEMRYEFHghAsCJffrx2BlPltelxKsq1EWRMA0GCSqGSIb3DQEBAQUABIGAuM7ucbK1FjMG1XBYCm7agr74JeC1UyZ7+elYAV1MRqBqhmjdogTlSBFvlZaOEYu0qJtCwTEG5oE639MFBhbxLhbVVitkJYIcLmIlzVQbMyrlCuUfkjgyVG2awHnpFngZ8SPIie4Ukp40arjc8QiKY2X45o0wUcAVFhnxIKFd5YE=-----END PKCS7-----">
-</input>
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-</input>
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</img>
-      </form>
-    </div>
-   </div>
-   </div>
+     <h4>Total Amount in USD: $10</h4>
+      <div ref={paypalRef} />
+       </div>
+       </div>
+     </div>
+   
   );
- }
 }
+

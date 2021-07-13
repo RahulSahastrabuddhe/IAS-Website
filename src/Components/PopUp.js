25c12bb2ace18ade8b2235f78275dbd6743eafd9
Rahul Bhalchandra Sahastrabuddhe <rahulsahastrabuddhe7@gmail.com>
2021-07-13 15:59:45 -0500

    added email functionality for payments

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -1,36 +1,26 @@
 import React from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-//import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "emailjs-com";
 
 export default function PopUp(props) {
   const [paid, setPaid] = React.useState(false);
   const [error] = React.useState(null);
   const paypalRef = React.useRef();
-  let dynamicClassName= "false";
-  let name = "";
-  let email = "";
+  // let dynamicClassName = "false";
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [netId, setNetId] = React.useState("");
+
+  const orderDetails = {
+    name: name,
+    email: email,
+    netId: netId,
+  };
+
   const handleClick = () => {
     props.toggle();
-   };
-
-   const handleNameChange = (e) => {
-     name = e;
-    console.log(name);
-    //  if(e == "")
-    //  {
-    //   dynamicClassName = "false";
-    //  }
-    //  else
-    //  {
-    //   dynamicClassName = "true";
-    //  }
-   };
-
-   const handleEmailChange = (e) => {
-    email = e;
-    console.log(email);
-   };
+  };
 
   // To show PayPal buttons once the component loads
   React.useEffect(() => {
@@ -56,7 +46,7 @@ export default function PopUp(props) {
           console.log(order);
         },
         onError: (err) => {
-        //   setError(err),
+          //   setError(err),
           console.error(err);
         },
       })
@@ -66,13 +56,23 @@ export default function PopUp(props) {
   // If the payment has been made
   if (paid) {
     toast("Payment Successfull. We will contact you soon:)");
-    // emailjs.sendForm('service_wb10qzj', 'template_macz4el', "dasd", 'user_EbsHvfEErVslPnqCBOmiN')
-    // .then((result) => {
-    //     console.log(result.text);
-    // }, (error) => {
-    //     console.log(error.text);
-    // });
-    // return null;
+    console.log(orderDetails);
+    emailjs
+      .send(
+        "service_wb10qzj",
+        "template_macz4el",
+        orderDetails,
+        "user_EbsHvfEErVslPnqCBOmiN"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    return null;
   }
 
   // If any error occurs
@@ -83,46 +83,64 @@ export default function PopUp(props) {
   // Default Render
   return (
     <div className="popup">
-    <ToastContainer/>
-    <div className="modal">
-     <div className="modal_content">
-     <span className="close" onClick={handleClick}>&times;    </span>
-     <h3>Become A Member</h3>
-     <h4>Total Amount in USD: $10</h4>
-     
-     <div>
-       
-                    <label htmlFor="contactName">
-                      Name <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="payerName"
-                      name="payerName"
-                      onChange={event => handleNameChange(event.target.value)}
-                    required/>
-                  </div>
+      <ToastContainer />
+      <div className="modal">
+        <div className="modal_content">
+          <span className="close" onClick={handleClick}>
+            &times;{" "}
+          </span>
+          <h3>Become A Member</h3>
+          <h4>Total Amount in USD: $10</h4>
+          <div>
+            <label htmlFor="contactName">
+              Name <span className="required">*</span>
+            </label>
+            <input
+              type="text"
+              defaultValue=""
+              size="35"
+              id="name"
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
 
-                  <div>
-                    <label htmlFor="contactEmail">
-                      Email <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="payerMail"
-                      name="payerName"
-                      onChange={event => handleEmailChange(event.target.value)}
-                    required/>
-                  </div>
-      <div className={dynamicClassName === "true"? "enabledBtn " : "disableBtn"} ref={paypalRef} />
-       </div>
-       </div>
-     </div>
-   
+          <div>
+            <label htmlFor="contactName">
+              NetID <span className="required">*</span>
+            </label>
+            <input
+              type="text"
+              defaultValue=""
+              size="35"
+              id="netId"
+              name="netId"
+              onChange={(e) => setNetId(e.target.value)}
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="contactEmail">
+              Email <span className="required">*</span>
+            </label>
+            <input
+              type="text"
+              defaultValue=""
+              size="35"
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          {/* className={
+                dynamicClassName === "true" ? "enabledBtn " : "disableBtn"
+              } */}
+          <div ref={paypalRef}></div>
+        </div>
+      </div>
+    </div>
   );
 }
-
